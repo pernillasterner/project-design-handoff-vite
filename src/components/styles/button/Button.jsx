74c6df8d789daa05d5de5d8bd1f987a35289ec97
Pernilla Sterner <psterner@hotmail.com>
2024-04-23 06:57:06 +0200

    Set general styling to buttons

--- src/components/styles/button/Button.jsx
+++ src/components/styles/button/Button.jsx
@@ -1,9 +1,5 @@
 import "./Button.css";
 
 export const Button = ({ label, classNames }) => {
-  return (
-    <button className={`${classNames || ""}`}>
-      <span className="label">{label}</span>
-    </button>
-  );
+  return <button className={`${classNames || ""}`}>{label}</button>;
 };
